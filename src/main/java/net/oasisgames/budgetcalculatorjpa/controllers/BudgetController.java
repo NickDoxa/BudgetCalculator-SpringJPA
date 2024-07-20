eebf18590cd77647ae12196b4a157d69ffb61ce7
nickdoxa <nick091702@gmail.com>
2024-07-19 22:51:22 -0400

    Add path variable to budget get mapping

--- src/main/java/net/oasisgames/budgetcalculatorjpa/controllers/BudgetController.java
+++ src/main/java/net/oasisgames/budgetcalculatorjpa/controllers/BudgetController.java
@@ -5,6 +5,7 @@ import net.oasisgames.budgetcalculatorjpa.repository.BudgetRepository;
 import net.oasisgames.budgetcalculatorjpa.services.BudgetService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -39,8 +40,8 @@ public class BudgetController {
      * @param user the users name
      * @return Budget information in JSON format
     */
-    @GetMapping("budget")
-    public BudgetInformation getBudgetData(String user) {
+    @GetMapping("budget/{user}")
+    public BudgetInformation getBudgetData(@PathVariable("user") String user) {
         return budgetRepository.findById(user).orElse(null);
     }
 
